73556c4108ebb8262f87c59d47e9ec9ac40cea05
MADMAN-Modding <mad@kmdtech.net>
2025-05-30 08:23:47 -0400

    starting to test getting url data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let _ = get_request().await.unwrap();
+}
+
+async fn get_request() -> Result<(), reqwest::Error> {
+    let body = reqwest::get("https://www.rust-lang.org")
+        .await?
+        .text()
+        .await?;
+
+    println!("body = {body:?}");
+
+    Ok(())
 }
